5d03ff1e8d5aba528ca5679112aea2d3f8049421
franck-benault <franck-benault@orange.fr>
2018-11-12 00:01:03 +0100

    [LOG] configure logging for first example

--- 01-simple-java-example/src/main/java/org/tapestry/example/pages/Index.java
+++ 01-simple-java-example/src/main/java/org/tapestry/example/pages/Index.java
@@ -1,8 +1,14 @@
 package org.tapestry.example.pages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Index {
 	
+	private static Logger logger = LoggerFactory.getLogger(Index.class);
+	
 	public String getUsername() {
+		logger.debug("getUsername called");
 		return "foo";
 	}
 
