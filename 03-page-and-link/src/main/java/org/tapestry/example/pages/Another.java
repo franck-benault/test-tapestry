d4172383f3dc0f3d4815cea0da843540cfd699cb
franck-benault <franck-benault@orange.fr>
2018-11-20 22:39:25 +0100

    [EXAMPLE] use pageLink component

--- 03-page-and-link/src/main/java/org/tapestry/example/pages/Another.java
+++ 03-page-and-link/src/main/java/org/tapestry/example/pages/Another.java
@@ -1,8 +1,13 @@
 package org.tapestry.example.pages;
 
+import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.corelib.components.PageLink;
 
 public class Another {
+	
+	@Component(parameters= {"page=Index"})
+	private PageLink linkToIndex;
 	
 	@Persist
 	private String passedMessage;
