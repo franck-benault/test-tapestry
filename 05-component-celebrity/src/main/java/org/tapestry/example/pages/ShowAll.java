fe654aca340d647dfa7b2fd3fca4ba9685b847cb
franck-benault <franck-benault@orange.fr>
2018-12-02 23:53:03 +0100

    [EXAMPLE] complete celebrity example

--- 05-component-celebrity/src/main/java/org/tapestry/example/pages/ShowAll.java
+++ 05-component-celebrity/src/main/java/org/tapestry/example/pages/ShowAll.java
@@ -1,8 +1,13 @@
 package org.tapestry.example.pages;
 
+import java.util.List;
+
 import org.apache.tapestry5.annotations.SessionState;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.tapestry.example.model.dto.Celebrity;
+import org.tapestry.example.model.management.IDataSource;
+import org.tapestry.example.model.management.impl.MockDataSource;
 import org.tapestry.example.model.security.User;
 
 public class ShowAll {
@@ -11,12 +16,19 @@ public class ShowAll {
 	
 	@SessionState(create=false)
 	private User user;
+	private Celebrity celebrity;
 	
-	
+	public Celebrity getCelebrity() {
+		return celebrity;
+	}
+
+	public void setCelebrity(Celebrity celebrity) {
+		this.celebrity = celebrity;
+	}
+
 	public User getUser() {
 		return user;
 	}
-
 
 	Object onActivate() {
 		if(user==null) {
@@ -24,6 +36,11 @@ public class ShowAll {
 		}
 		logger.info("user ={}", user);
 		return null;
+	}
+	
+	public List<Celebrity> getAllCelebrities() {
+		IDataSource dataSource = new MockDataSource();
+		return dataSource.getAllCelebries();
 	}
 
 }
