abb73b02656fad4b7466b6164fb0ca0d34a28aab
franck-benault <franck-benault@orange.fr>
2018-12-25 00:05:26 +0100

    [EXAMPLE] submit form

--- 05-component-celebrity/src/main/java/org/tapestry/example/pages/Registration.java
+++ 05-component-celebrity/src/main/java/org/tapestry/example/pages/Registration.java
@@ -1,5 +1,7 @@
 package org.tapestry.example.pages;
 
+import org.apache.tapestry5.EventConstants;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.tapestry.example.model.security.Gender;
@@ -31,6 +33,8 @@ public class Registration {
 	@Property
 	private boolean unsubscribe;
 	
+	private String action;
+	
 	
 	public Gender getMale() {
 		return Gender.MALE;
@@ -40,11 +44,31 @@ public class Registration {
 		return Gender.FEMALE;
 	}
 	
-	void onSubmitFromRegistrationForm() {
+	@OnEvent(component = "submitButton", value = EventConstants.SELECTED)
+	void submitButtonClicked() {
+		System.out.println("submit button clicked...");
+	    action = "buttonSubmit";
+	}
+	
+	@OnEvent(component="registrationForm" ,value = EventConstants.SUCCESS)
+	public void success() {
+
+		System.out.println("The form has been successed with action "+action);
+	}
+	
+	@OnEvent(value=EventConstants.SUBMIT, component="registrationForm")
+	public void onSubmitButton() {
+		System.out.println("The form has been submitted with action "+action);
 		System.out.println("The form has been submitted with gender "+gender);
 		System.out.println("The form has been submitted with unsubscribe "+unsubscribe);
 		
 		if(unsubscribe) subscribe = false;
+		
+		if(action!=null) {
+			
+		}
 	}
+	
+
 
 }
