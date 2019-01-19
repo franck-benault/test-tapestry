a9ded2110d3c93be25233e8e828cbcd87a9e1c51
franck-benault <franck-benault@orange.fr>
2019-01-19 20:02:57 +0100

    [EXAMPLE] add css

--- 05-component-celebrity/src/main/java/org/tapestry/example/pages/AddCelebrity.java
+++ 05-component-celebrity/src/main/java/org/tapestry/example/pages/AddCelebrity.java
@@ -1,9 +1,11 @@
 package org.tapestry.example.pages;
 
+import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.tapestry.example.model.dto.Celebrity;
 
+@Import(stylesheet="context:css/style.css")
 public class AddCelebrity {
 	
 	@Property
